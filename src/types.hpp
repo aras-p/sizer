c8a9e2cc89e2d8703aaaae198344ab740b6cb2cb
Aras Pranckevicius <aras@nesnausk.org>
2023-08-02 13:26:13 +0300

    Cleanup: http -> https

--- src/types.hpp
+++ src/types.hpp
@@ -1,5 +1,5 @@
 // Executable size report utility.
-// Aras Pranckevicius, http://aras-p.info/projSizer.html
+// Aras Pranckevicius, https://aras-p.info/projSizer.html
 // Based on code by Fabian "ryg" Giesen, http://farbrausch.com/~fg/
 // Public domain.
 
